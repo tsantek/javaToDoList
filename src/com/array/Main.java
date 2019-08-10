f59c4e8306b0d50107b04cacc7ce2dc9a57c2a2a
tsantek <santek.vg@gmail.com>
2019-08-10 11:23:16 -0700

    finish list

--- src/com/array/Main.java
+++ src/com/array/Main.java
@@ -42,8 +42,37 @@ public class Main {
     }
 
     public  static void printInstructions(){
-        System.out.println("Press: \n 0 - To print options  \n 1 - To print list  \n 2 - to add item \n 3 - to modify item \4 to remove item \n 5 = to search item \n 6 to exit");
+        System.out.println("Press: \n 0 - To print options  \n 1 - To print list  \n 2 - to add item \n 3 - to modify item \n 4 to remove item \n 5 = to search item \n 6 to exit");
     }
 
+    public static void addItem(){
+        System.out.println("Please enter item");
+        list.addItem(scanner.nextLine());
+    }
 
+    public static void removeItem(){
+        System.out.println("Please enter item No to remove it");
+        int itemNo = scanner.nextInt();
+        scanner.nextLine();
+        list.removeItem(itemNo-1);
+    }
+
+    public static void  modifyItem(){
+        System.out.println("Please enter item No to remove it");
+       int itemNo = scanner.nextInt();
+       scanner.nextLine();
+        System.out.println("Enter replacemt Item :" );
+        String newItem = scanner.nextLine();
+        list.modifyItem(itemNo-1, newItem);
+    }
+
+    public static void findItem(){
+        System.out.println("Please search for item ");
+        String searchItem = scanner.nextLine();
+        if(list.findItem(searchItem) != null){
+            System.out.println("Found " + searchItem);
+        }else{
+            System.out.println("Item not found");
+        }
+    }
 }
